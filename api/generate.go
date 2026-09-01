0f6e7aa143b5f37cf49f02c85b0ce7d2a3e1d068
MI-1222 <rohisamari1222@gmail.com>
2026-09-01 12:00:48 +0900

    feat: openAPIからコード自動生成

---
+++ api/generate.go
@@ -0,0 +1,4 @@
+// Package api は OpenAPI 3.1 スキーマおよびコード自動生成を管理するパッケージ。
+package api
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-codegen.yaml openapi.yaml
